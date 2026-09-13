72293ce3622b40756afea199345edd37c49c9786
Micheal <lucky11kononov@yandex.ru>
2026-09-13 23:15:16 +0200

    VP-1: Удаляет лишний код

--- config/asgi.py
+++ config/asgi.py
@@ -5,17 +5,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
-from chat.middleware import JWTAuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
-    }
-)
